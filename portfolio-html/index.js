4ed94f511dfbccceebb15926dd58f579c0d2e628
Vigneshwar <vigneshwar.s1996@gmail.com>
2026-01-17 19:42:09 +0530

    feat: enhance banner section with Atropos integration and improved styling

--- portfolio-html/index.js
+++ portfolio-html/index.js
@@ -1,3 +1,6 @@
+
+import Atropos from 'https://cdn.jsdelivr.net/npm/atropos@2/atropos.min.mjs';
+
 $(document).ready(function() {
   // Wait for all animations to complete (last letter at 0.4s + 0.4s animation + 0.5s pause)
   setTimeout(function() {
@@ -8,4 +11,13 @@ $(document).ready(function() {
       $('.loader').remove();
     }, 1000);
   }, 1300);
+});
+
+
+
+const myAtropos = Atropos({
+  el: '.my-atropos',
+  activeOffset: 40,
+  shadowScale: 1.05,
+  // rest of parameters
 });
